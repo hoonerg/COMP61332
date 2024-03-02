e7a3f9b99dde1eda4b5e81a28220922b62c16d8c
Sehee Kim <seheekim618@gmail.com>
2024-03-02 23:21:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from training.train import main as training_main
-from test.predict_on_test_dataset import predict
+import sys
+from train import main as training_main
+from test import predict
 import pandas as pd
+
 pd.set_option('display.width', 1000)
 
-training_main()
-predict()
+if __name__ == "__main__":
+    model_type = sys.argv[1] if len(sys.argv) > else None
+
+    training_main(model_type)
+    predict(model_type)
