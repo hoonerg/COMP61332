d8cab136cd4c775658fff77933863c7116d405f6
Sehee Kim <seheekim618@gmail.com>
2024-03-04 12:02:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ pd.set_option('display.width', 1000)
 
 if __name__ == "__main__":
     model_type = sys.argv[1] if len(sys.argv) > 1 else None
+    user_input = ' '.join(sys.argv[2:]) if len(sys.argv) > 2 else None
 
-    training_main(model_type)
-    predict(model_type)
+   #training_main(model_type)
+
+    if len(sys.argv) > 2:
+        predict(model_type, user_input)
+
+    else:
+        training_main(model_type)
+        predict(model_type, user_input)
