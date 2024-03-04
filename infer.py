6087d94d07002a9b6784e9b7d268bbb9f0d2cac4
hoonerg <hoonchoi137@gmail.com>
2024-03-04 17:05:53 +0000

    Update inference

--- infer.py
+++ infer.py
@@ -6,20 +6,16 @@ import pandas as pd
 pd.set_option('display.width', 1000)
 
 def get_user_input():
-    # Prompt for user inputs
     sentence = input("Enter the sentence: ")
     first_entity = input("Enter the first entity: ")
     second_entity = input("Enter the second entity: ")
     
-    # Format the user input as required
     formatted_input = f'"{sentence}", "{first_entity}", "{second_entity}"'
     return formatted_input
 
 if __name__ == "__main__":
     user_input = get_user_input()
     
-    #print("Predicting with SVM model...")
-    #predict("SVM", user_input)
+    #predict("Predicting with SVM", user_input)
     
-    print("\nPredicting with LSTM model...")
-    predict("LSTM", user_input)
+    predict("Predicting with LSTM ", user_input)
