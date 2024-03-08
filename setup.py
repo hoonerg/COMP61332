fd52ba9ed41676fc51add73f5c3ea517710ce7ea
Hoon Choi <hoonchoi137@gmail.com>
2024-03-08 23:23:23 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,3 +7,11 @@ gdown.download(id=id, output=output)
 output = "results/checkpoints/svm_best_model.pkl"
 id = "1ZdTVBXUFPS5UUXiHwSH0u2MbWWAaCyFY"
 gdown.download(id=id, output=output)
+
+output = "label_encoder.pkl"
+id = "1V9V3qvhu88Fc2t5uYsE6s80TuIL6ATJI"
+gdown.download(id=id, output=output)
+
+output = "vocab.pkl"
+id = "1YOrQquwlXr46LlIO-RzhvY_ZrjYB71qM"
+gdown.download(id=id, output=output)
